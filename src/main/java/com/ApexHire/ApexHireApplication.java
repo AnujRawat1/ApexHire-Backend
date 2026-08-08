93e157c7798f387fd569777cb35ce16a7894d311
AnujRawat1 <rawatanuj058gmail.com>
2026-08-08 19:23:13 +0530

    Backend Base Setup initialize.

---
+++ src/main/java/com/ApexHire/ApexHireApplication.java
@@ -0,0 +1,13 @@
+package com.ApexHire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexHireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApexHire/ApexHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApexHire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
